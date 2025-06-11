19da7873b45b86de1e8462d193c9d6ad8b1a3f1a
reypowgithub <reypowofc@gmail.com>
2025-06-11 14:45:14 +0700

    fix : mengubah icons jadi MeterialIcons

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import NavigationBar from './src/components/NavigationBar';
-import AccountBalance from './src/components/AccountBalance';
-import QuickActions from './src/components/QuickActions';
-import StepProgressBar from './src/components/ProgressBar';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import NavigationBar from "./src/components/NavigationBar";
+import AccountBalance from "./src/components/AccountBalance";
+import QuickActions from "./src/components/QuickActions";
+import StepProgressBar from "./src/components/ProgressBar";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <NavigationBar onMenuPress={() => console.log('Menu pressed')} />
+      <NavigationBar onMenuPress={() => console.log("Menu pressed")} />
       <AccountBalance balance={10000000} />
       <QuickActions />
-      <StepProgressBar currentStep={3} />
+      <StepProgressBar currentStep={2} />
     </View>
   );
 }
@@ -20,6 +20,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5F5F5',
+    backgroundColor: "#F5F5F5",
   },
 });
