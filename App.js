df04fa8ce10ef50b77ea70d2c7cd250e34daddfe
zhirazzi <zhirazzi@gmail.com>
2025-06-12 10:48:50 +0700

    feat onboarding & success page

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import OTPScreen from "./src/screens/OTPScreen";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import NavigationBar from './src/components/NavigationBar';
+import AccountBalance from './src/components/AccountBalance';
+import QuickActions from './src/components/QuickActions';
+import Onboarding from './src/screens/e-KYC/Onboarding';
+import Success from './src/screens/e-KYC/Success';
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={styles.container}>
+    <View style={styles.container}>
       <StatusBar style="light" />
-      <OTPScreen />
-    </GestureHandlerRootView>
+      {/* <NavigationBar onMenuPress={() => console.log('Menu pressed')} />
+      <AccountBalance balance={10000000} />
+      <QuickActions /> */}
+      <Onboarding />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F5F5F5",
+    backgroundColor: '#F5F5F5',
   },
 });
