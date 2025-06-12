b5c6487b2b9af9d187b7ff8c6878d0480d73f54e
reypowgithub <reypowofc@gmail.com>
2025-06-12 08:46:08 +0700

    feat : make lampiran page

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import OTPScreen from "./src/screens/OTPScreen";
+// import OTPScreen from "./src/screens/OTPScreen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+// import Welcoming from "./src/screens/e-KYC/Welcoming";
+import Lampiran from "./src/screens/e-KYC/KYC_Lampiran";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <StatusBar style="light" />
-      <OTPScreen />
+      <Lampiran/>
     </GestureHandlerRootView>
   );
 }
