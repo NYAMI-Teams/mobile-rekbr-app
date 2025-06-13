daa4856fa087d39619ae664231ce34271e1857b6
zhirazzi <zhirazzi@gmail.com>
2025-06-13 20:56:50 +0700

    refactor choose rekening

--- App.js
+++ App.js
@@ -1,33 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import NavigationBar from "./src/components/NavigationBar";
-import AccountBalance from "./src/components/AccountBalance";
-import QuickActions from "./src/components/QuickActions";
-import Onboarding from "./src/screens/e-KYC/Onboarding";
-import Success from "./src/screens/e-KYC/Success";
-import Seller from "./src/screens/seller/homeScreen";
-import BuyerEmptyContent from "./src/screens/buyer";
-import "./global.css";
-import BankScreen from "./src/screens/bank";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import NavigationBar from './src/components/NavigationBar';
+import AccountBalance from './src/components/AccountBalance';
+import QuickActions from './src/components/QuickActions';
+import Onboarding from './src/screens/e-KYC/Onboarding';
+import ChooseRekening from './src/screens/seller/chooseRekening';
 
 export default function App() {
-  const bankData = {
-    accountHolder: "Sdr Bayu Saptaji Rahman",
-    bankName: "Bank Negara Indonesia",
-    accountNumber: "0900604501",
-    // Gunakan require untuk gambar lokal di React Native
-    logoSrc: require("./assets/bni-logo2.png"), // Sesuaikan path sesuai lokasi gambar Anda
-  };
-
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      {/* <NavigationBar /> */}
       {/* <NavigationBar onMenuPress={() => console.log('Menu pressed')} />
       <AccountBalance balance={10000000} />
       <QuickActions /> */}
-      {/* <BuyerEmptyContent /> */}
-      <BankScreen/>
+      <ChooseRekening />
     </View>
   );
 }
