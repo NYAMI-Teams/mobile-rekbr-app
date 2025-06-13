4f6cf971f1d19d58ba86575b75e4c89870e86594
zhirazzi <zhirazzi@gmail.com>
2025-06-13 11:04:52 +0700

    feat choose rekening

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import NavigationBar from "./src/components/NavigationBar";
-import AccountBalance from "./src/components/AccountBalance";
-import QuickActions from "./src/components/QuickActions";
-import Onboarding from "./src/screens/e-KYC/Onboarding";
-import Success from "./src/screens/e-KYC/Success";
-import Seller from "./src/screens/seller/homeScreen";
-import BuyerEmptyContent from "./src/screens/buyer";
-import "./global.css";
-import SellerCard from "./src/components/card-transaction/SellerCard";
-import BuyerCard from "./src/components/card-transaction/BuyerCard";
-import Toast from "react-native-toast-message";
-import { mockAPIBuyer, mockAPISeller } from "./src/services/apiMock/api";
-import History from "./src/screens/history";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import NavigationBar from './src/components/NavigationBar';
+import AccountBalance from './src/components/AccountBalance';
+import QuickActions from './src/components/QuickActions';
+import Onboarding from './src/screens/e-KYC/Onboarding';
+import ChooseRekening from './src/screens/seller/chooseRekening';
+import "./global.css"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="dark" />
-      {/* Hapus Dari sini (TULISAN INI JANGAN DI HAPUS) */}
-      <View className="gap-4">
-        <History />
-      </View>
-      {/* Hapus Sampai sini (TULISAN INI JANGAN DI HAPUS) */}
+      <StatusBar style="light" />
+      {/* <NavigationBar onMenuPress={() => console.log('Menu pressed')} />
+      <AccountBalance balance={10000000} />
+      <QuickActions /> */}
+      <ChooseRekening />
     </View>
   );
 }
@@ -30,6 +22,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: '#F5F5F5',
   },
 });
